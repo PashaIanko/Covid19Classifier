ca6cd6392155952d3eb066c15dcf583ccec8c484
Pavel Ianko <yanpavevg@yandex.ru>
2022-01-22 22:53:16 +0100

    splitted data

--- DataProperties.py
+++ DataProperties.py
@@ -19,6 +19,10 @@ class DataProperties:
         self.train_pneumonia_path = self.train_data_path + 'pneumonia/'
         self.train_healthy_path = self.train_data_path + 'normal/'
 
+        self.val_covid_path = self.val_data_path + 'covid/'
+        self.val_pneumonia_path = self.val_data_path + 'pneumonia/'
+        self.val_healthy_path = self.val_data_path + 'normal/'
+
         self.test_covid_path = self.test_data_path + 'covid/'
         self.test_pneumonia_path = self.test_data_path + 'pneumonia/'
         self.test_healthy_path = self.test_data_path + 'normal/'
@@ -28,7 +32,9 @@ class DataProperties:
             self.checkpoint_path = 'C:/Users/79137/Pasha/2. UNIPD/HDA/Project/Checkpoints/'
             self.train_data_path = 'C:/Users/79137/Pasha/2. UNIPD/HDA/Project/Data/TrainData/'
             self.test_data_path = 'C:/Users/79137/Pasha/2. UNIPD/HDA/Project/Data/TestData/'
+            self.val_data_path = 'C:/Users/79137/Pasha/2. UNIPD/HDA/Project/Data/ValidationData/'
         elif self.envorinment == 'colab':
             self.checkpoint_path = '/content/drive/MyDrive/UNIPD/HDA/Project/Checkpoints/'
             self.train_data_path = '/content/drive/MyDrive/UNIPD/HDA/Project/TrainData/'
             self.test_data_path = '/content/drive/MyDrive/UNIPD/HDA/Project/TestData/'
+            self.val_data_path =  '/content/drive/MyDrive/UNIPD/HDA/Project/ValidationData/'
