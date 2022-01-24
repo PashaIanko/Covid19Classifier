87e418442f525cfbf5ec277bdcabd062c1a382ba
Pavel Ianko <yanpavevg@yandex.ru>
2022-01-24 22:00:17 +0100

    all works, more generalized code

--- Utils.py
+++ Utils.py
@@ -225,7 +225,7 @@ def collect_metrics(models_dict, data_flow, data_steps):
         trainable_params = np.sum(
             [np.prod(v.get_shape()) for v in model.model.trainable_weights]
         )
-        res_dict[name]['tr_params'] = trainable_params
+        res_dict[name]['tr_params'] = int(trainable_params)
         # nonTrainableParams = np.sum([np.prod(v.get_shape()) for v in model.non_trainable_weights])
         # totalParams = trainableParams + nonTrainableParams
         
