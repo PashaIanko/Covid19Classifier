8e71409a06773e176cbe4e5bef7158d6358c7c41
Pavel Ianko <yanpavevg@yandex.ru>
2022-01-27 18:11:27 +0100

    removed bilateral filter

--- Preprocessing.py
+++ Preprocessing.py
@@ -8,11 +8,11 @@ def preprocess(image):
     image = image / 255.
     image = equalize_adapthist(image, clip_limit = 0.015)
     
-    image = bilateralFilter(
-        image,
-        d = 2, #PreprocessingParameters.d,  # kernel
-        sigmaColor = 12, # PreprocessingParameters.sigma_color,  # Gauss blur component
-        sigmaSpace = 2# PreprocessingParameters.sigma_space  # Range component
-    )
+    # image = bilateralFilter(
+    #     image,
+    #     d = 2, #PreprocessingParameters.d,  # kernel
+    #     sigmaColor = 12, # PreprocessingParameters.sigma_color,  # Gauss blur component
+    #     sigmaSpace = 2# PreprocessingParameters.sigma_space  # Range component
+    # )
 
     return image
