b3630cc2c3e551f5cc39d7a0719883e853e49a75
Pavel Ianko <yanpavevg@yandex.ru>
2022-01-31 14:55:53 +0100

    removed trash

--- AlexNetModel.py
+++ AlexNetModel.py
@@ -37,47 +37,6 @@ class AlexNetModel(Model):
         model.add(layers.Dense(3, activation= 'softmax'))
 
         self.model = model
-
-        # inputs = layers.Input(
-        #     shape = PreprocessingParameters.target_shape + \
-        #            PreprocessingParameters.n_color_channels
-        # )
-
-        # resize = tf.keras.layers.Lambda(
-        #     lambda image: tf.image.resize(
-        #         image,
-        #         (256, 256),
-        #         preserve_aspect_ratio = True
-        #     )
-        # )(inputs)
-
-        # x = layers.Conv2D(96, 11, strides=4, padding='same', input_shape = (256, 256, 3))(resize)
-        # x = layers.Lambda(tf.nn.local_response_normalization)(x)
-        # x = layers.Activation('relu')(x)
-        # x = layers.MaxPooling2D(3, strides=2)(x)
-
-        # x = layers.Conv2D(256, 5, strides=4, padding='same')(x)
-        # x = layers.Lambda(tf.nn.local_response_normalization)(x)
-        # x = layers.Activation('relu')(x)
-        # x = layers.MaxPooling2D(3, strides=2)(x)
-
-        # x = layers.Conv2D(384, 3, strides=4, padding='same')(x)
-        # x = layers.Activation('relu')(x)
-
-        # x = layers.Conv2D(384, 3, strides=4, padding='same')(x)
-        # x = layers.Activation('relu')(x)
-
-        # x = layers.Conv2D(256, 3, strides=4, padding='same')(x)
-        # x = layers.Activation('relu')(x)
-        
-        # x = layers.Flatten()(x)
-        # x = layers.Dense(4096, activation='relu')(x)
-        # x = layers.Dropout(0.5)(x)
-        # x = layers.Dense(4096, activation='relu')(x)
-        # x = layers.Dropout(0.5)(x)
-        # predictions = layers.Dense(DataProperties.n_classes, activation='softmax')(x)
-
-        # self.model = tf_Model(inputs = inputs, outputs = predictions)
        
 
 
