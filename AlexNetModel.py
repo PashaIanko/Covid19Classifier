3954cea9a60dab88cce9e06bc41b4c8f2bd17eaf
Pavel Ianko <yanpavevg@yandex.ru>
2022-01-31 12:39:36 +0100

    Alexnet fits well

--- AlexNetModel.py
+++ AlexNetModel.py
@@ -17,46 +17,67 @@ class AlexNetModel(Model):
 
     def construct_model(self):
 
-        inputs = layers.Input(
-            shape = PreprocessingParameters.target_shape + \
-                   PreprocessingParameters.n_color_channels
-        )
+        model = tf.keras.models.Sequential()
 
-        resize = tf.keras.layers.Lambda(
-            lambda image: tf.image.resize(
-                image,
-                (256, 256),
-                preserve_aspect_ratio = True
-            )
-        )(inputs)
+        model.add(layers.Conv2D(filters = 96, kernel_size = (11, 11), strides = 4, padding = 'valid', activation = 'relu', input_shape = (256, 256, 3), kernel_initializer = 'he_normal'))
+        model.add(layers.MaxPooling2D(pool_size = (3, 3), strides = (2, 2), padding = 'valid', data_format = None))
 
-        x = layers.Conv2D(96, 11, strides=4, padding='same', input_shape = (256, 256, 3))(resize)
-        x = layers.Lambda(tf.nn.local_response_normalization)(x)
-        x = layers.Activation('relu')(x)
-        x = layers.MaxPooling2D(3, strides=2)(x)
+        model.add(layers.Conv2D(256, kernel_size=(5,5), strides= 1, padding= 'same', activation= 'relu', kernel_initializer= 'he_normal'))
+        model.add(layers.MaxPooling2D(pool_size=(3,3), strides= (2,2), padding= 'valid', data_format= None))
 
-        x = layers.Conv2D(256, 5, strides=4, padding='same')(x)
-        x = layers.Lambda(tf.nn.local_response_normalization)(x)
-        x = layers.Activation('relu')(x)
-        x = layers.MaxPooling2D(3, strides=2)(x)
+        model.add(layers.Conv2D(384, kernel_size=(3,3), strides= 1, padding= 'same', activation= 'relu', kernel_initializer= 'he_normal'))
+        model.add(layers.Conv2D(384, kernel_size=(3,3), strides= 1, padding= 'same', activation= 'relu', kernel_initializer= 'he_normal'))
+        model.add(layers.Conv2D(256, kernel_size=(3,3), strides = 1, padding= 'same', activation= 'relu', kernel_initializer= 'he_normal'))
+        model.add(layers.MaxPooling2D(pool_size=(3,3), strides= (2,2), padding= 'valid', data_format= None))
 
-        x = layers.Conv2D(384, 3, strides=4, padding='same')(x)
-        x = layers.Activation('relu')(x)
+        model.add(layers.Flatten())
+        model.add(layers.Dense(4096, activation= 'relu'))
+        model.add(layers.Dense(4096, activation= 'relu'))
+        model.add(layers.Dense(1000, activation= 'relu'))
+        model.add(layers.Dense(3, activation= 'softmax'))
 
-        x = layers.Conv2D(384, 3, strides=4, padding='same')(x)
-        x = layers.Activation('relu')(x)
+        self.model = model
 
-        x = layers.Conv2D(256, 3, strides=4, padding='same')(x)
-        x = layers.Activation('relu')(x)
+        # inputs = layers.Input(
+        #     shape = PreprocessingParameters.target_shape + \
+        #            PreprocessingParameters.n_color_channels
+        # )
+
+        # resize = tf.keras.layers.Lambda(
+        #     lambda image: tf.image.resize(
+        #         image,
+        #         (256, 256),
+        #         preserve_aspect_ratio = True
+        #     )
+        # )(inputs)
+
+        # x = layers.Conv2D(96, 11, strides=4, padding='same', input_shape = (256, 256, 3))(resize)
+        # x = layers.Lambda(tf.nn.local_response_normalization)(x)
+        # x = layers.Activation('relu')(x)
+        # x = layers.MaxPooling2D(3, strides=2)(x)
+
+        # x = layers.Conv2D(256, 5, strides=4, padding='same')(x)
+        # x = layers.Lambda(tf.nn.local_response_normalization)(x)
+        # x = layers.Activation('relu')(x)
+        # x = layers.MaxPooling2D(3, strides=2)(x)
+
+        # x = layers.Conv2D(384, 3, strides=4, padding='same')(x)
+        # x = layers.Activation('relu')(x)
+
+        # x = layers.Conv2D(384, 3, strides=4, padding='same')(x)
+        # x = layers.Activation('relu')(x)
+
+        # x = layers.Conv2D(256, 3, strides=4, padding='same')(x)
+        # x = layers.Activation('relu')(x)
         
-        x = layers.Flatten()(x)
-        x = layers.Dense(4096, activation='relu')(x)
-        x = layers.Dropout(0.5)(x)
-        x = layers.Dense(4096, activation='relu')(x)
-        x = layers.Dropout(0.5)(x)
-        predictions = layers.Dense(DataProperties.n_classes, activation='softmax')(x)
+        # x = layers.Flatten()(x)
+        # x = layers.Dense(4096, activation='relu')(x)
+        # x = layers.Dropout(0.5)(x)
+        # x = layers.Dense(4096, activation='relu')(x)
+        # x = layers.Dropout(0.5)(x)
+        # predictions = layers.Dense(DataProperties.n_classes, activation='softmax')(x)
 
-        self.model = tf_Model(inputs = inputs, outputs = predictions)
+        # self.model = tf_Model(inputs = inputs, outputs = predictions)
        
 
 
